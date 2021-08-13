f02bce5f3731b66b6f7e3701091edadd1950170c
Deweyfang <Deweyfang@gmail.com>
2021-08-13 00:58:49 -0700

    update doc for decision engine, unit test passed

--- decision_engine.py
+++ decision_engine.py
@@ -6,17 +6,56 @@ from fetch_class import FetchConfig
 import time
 
 class DecisionEngine:
+  """ Main Decision Engine Class, Core of the system
+
+  This class provides the implementation of decision engine it can handle predefined 
+  operation and potentially support customizable low code environment for client to 
+  build customized pipline
+
+  TODO: Adding low code customizable pipline config interface 
+
+  Typical usage example:
+  
+  myengine = DecisionEngine()
+  myengine.init_fetch_pool(n = 2, ip_list = ['localhost:50051', 'localhost:50052'])
+  myengine.init_kinova_pool(n = 2, ip_list = ['localhost:50053', 'localhost:50054'])
+
+  fetch_checkout_config = FetchConfig(operation="CheckOut", kit_ID=1, kit_location=255, target_location=255)
+  kinova_checkout_config = KinovaConfig(operation="CheckOut", kit_ID=1, item_list=[2,4,6])
+
+  fetch_checkin_config1 = FetchConfig(operation="CheckIn", kit_ID=2, kit_location=128, target_location=128)
+  kinova_checkin_config1 = KinovaConfig(operation="CheckIn", kit_ID=2, item_list=[1,3,5])
+
+  fetch_checkin_config2 = FetchConfig(operation="CheckIn", kit_ID=3, kit_location=64, target_location=64)
+  kinova_checkin_config2 = KinovaConfig(operation="CheckIn", kit_ID=3, item_list=[7,8,9])
+
+  myengine.op_CheckIn(fetch_checkin_config1, kinova_checkin_config1)
+  myengine.op_CheckIn(fetch_checkin_config2, kinova_checkin_config2)
+  myengine.op_CheckOut(fetch_checkout_config, kinova_checkout_config)
+  myengine.op_CheckOut(fetch_checkout_config, kinova_checkout_config)
+  myengine.op_CheckOut(fetch_checkout_config, kinova_checkout_config)
+  """
   def __init__(self) -> None:
-      pass
+    """
+      Init function
+    """
+    pass
 
   def init_fetch_pool(self, n : int, ip_list : List):
+    """
+      Init the fetch_pool object
+    """
     self.fetch_pool_ = Fetch_pool(n, ip_list)
   
   def init_kinova_pool(self, n : int, ip_list : List):
+    """
+      Init the kinova_pool object
+    """
     self.kinova_pool_ = Kinova_pool(n, ip_list)
 
   def op_CheckOut(self, fetch_config : FetchConfig, kinova_config : KinovaConfig):
-    """ predefined operation
+    """ 
+      predefined operation
     """
     fetch = self.fetch_pool_.get_fetch()
     kinova = self.kinova_pool_.get_kinova() 
@@ -35,7 +74,8 @@ class DecisionEngine:
     self.kinova_pool_.return_kinova(kinova)
 
   def op_CheckIn(self, fetch_config : FetchConfig, kinova_config : KinovaConfig):
-    """ predefined operation
+    """ 
+      predefined operation
     """
     fetch = self.fetch_pool_.get_fetch()
     kinova = self.kinova_pool_.get_kinova()
@@ -54,6 +94,7 @@ class DecisionEngine:
     self.kinova_pool_.return_kinova(kinova)
 
 if __name__ == "__main__":
+  # Make sure fetch_server.py and kinova_server.py is running first
   myengine = DecisionEngine()
   myengine.init_fetch_pool(n = 2, ip_list = ['localhost:50051', 'localhost:50052'])
   myengine.init_kinova_pool(n = 2, ip_list = ['localhost:50053', 'localhost:50054'])
